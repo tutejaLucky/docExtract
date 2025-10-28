5f35303eda477e22424bed90f36e19b5d039a9cf
Lovish Tuteja <lovish.kumar@mscorpres.in>
2025-10-28 18:09:58 +0530

    edit app

--- app.py
+++ app.py
@@ -52,7 +52,10 @@ def upload_file():
         }
     })
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     os.makedirs("uploads", exist_ok=True)
     os.makedirs("outputs", exist_ok=True)
-    app.run(debug=True)
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
